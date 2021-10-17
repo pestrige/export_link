aa1b7e8b188b6d7899a0d181bccbdcbe664b9208
pestrige <pestrige@gmail.com>
2021-10-17 20:36:05 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './layouts/Header/Header';
+import Footer from './layouts/Footer/Footer';
+import Content from './layouts/Content/Content';
+import FormSection from './components/FormSection/FormSection';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
+const App = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Content>
+        <FormSection />
+        <ToastContainer theme='colored'/>
+      </Content>
+      <Footer />
+    </>
   );
 }
 
