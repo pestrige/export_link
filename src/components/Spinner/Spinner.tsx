2a718b5482882057a65cec0d6fc94cf4fdd15c85
pestrige <pestrige@gmail.com>
2021-10-18 10:01:20 +0300

    remove react-spinners

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import RingLoader from 'react-spinners/RingLoader';
 import styles from './Spinner.module.scss';
 
 const Spinner = () => {
   return (
-    <div className={styles.wrapper}>
-      <RingLoader loading size={60} color='#0071B8' />
+    <div className={styles.overlay}>
+      <div className={styles.wrapper}>
+        <span className={styles.ring1} />
+        <span className={styles.ring2} />
+      </div>
     </div>
   );
 };
